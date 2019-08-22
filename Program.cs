4bc9c47e4b65cc609e292f5a5f31d3452726bf30
nullxDEADBEEF <andxdgu@gmail.com>
2019-08-23 01:55:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DodgerCS {
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new DodgerGame())
+                game.Run();
+        }
+    }
+#endif
+}
